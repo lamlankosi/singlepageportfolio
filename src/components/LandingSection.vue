2d8c44a77953984f4cd743a04152616e862a5ef7
lamlankosi <lamlanomnganga9@gmail.com>
2024-06-26 16:19:49 +0200

    landingPage

---
+++ src/components/LandingSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="https://lamlankosi.github.io/project_images/Images/LN_logo.png" alt="landing">
+        </div>
+        <div class="col">
+            <div id="details">
+                <h1 class="display-1">Lamla Nomnganga</h1>
+                <span>{{ jobTitle[0].title }}</span>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    import { computed, onMounted, onMounyed} from 'vue'
+    const jobTitle = computed(() => this.$store.state.jobTitle)
+    onMounted(() => {
+        this.$store.dispatch('fetchJobTitle')
+    }) 
+</script>
+
+<style>
+
+</style>
