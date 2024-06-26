3734809a1ddd6125c14f0b990196cf21d6b522ae
lamlankosi <lamlanomnganga9@gmail.com>
2024-06-26 12:33:06 +0200

    cardWithSlots

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+        <slot name="cardHeader"></slot>
+        <div class="card-body">
+            <slot name="cardBody"></slot>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'cardComp'
+    }
+</script>
+    
+
+<style>
+    .card{
+        width: 18rem;
+    }
+</style>
